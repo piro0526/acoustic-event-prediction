aba34fc004ca71ab36f6236cfc219e114491cf12
piro0526 <piro0526@gmail.com>
2025-12-29 19:17:34 +0000

    first commit

---
+++ src/moshi/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) Kyutai, all rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+moshi is the inference codebase for Kyutai audio generation models.
+
+The code has been adapted from Audiocraft, see LICENSE.audiocraft
+  Copyright (c) Meta Platforms, Inc. and affiliates.
+"""
+
+# flake8: noqa
+from . import conditioners
+from . import models
+from . import modules
+from . import quantization
+from . import utils
+from . import prefetching
+
+__version__ = "0.2.12a3"
